fe3c6e05784cfaa1b477b983d3d095796607c13f
Nikola <nikolad991@gmail.com>
2023-09-09 22:43:50 +0200

    Footer, Error Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,53 @@
 import React from "react";
+import {
+  AiFillFacebook,
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillTwitterSquare,
+} from "react-icons/ai";
+import { BiSolidMoviePlay } from "react-icons/bi";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const watchlist = useSelector((state) => state.watchlist);
+  return (
+    <div className="bg-neutral-800 text-white  p-4 w-full flex flex-col gap-3 items-center justify-center">
+      <div className="bg-gradient-to-r from-neutral-600 to-yellow-800 rounded-md p-5">
+        <div className="flex items-center w-fit font-bold text-black text-2xl font-dosis border-b-4 border-t-4 border-dashed border-black bg-yellow-700 p-1">
+          <BiSolidMoviePlay /> <span>MOVIE_DB</span>
+        </div>
+      </div>
+
+      <div className="flex gap-8 hover:[&>a]:text-red-500 [&>a]:transition [&>a]:duration-500 ">
+        <Link to="/">Home</Link>
+
+        <Link to="/watchlist">My Watchlist({watchlist.length})</Link>
+      </div>
+      <div className="flex gap-3 items-center hover:[&>a]:text-red-500 [&>a]:transition [&>a]:duration-500 ">
+        <a href="https://instagram.com" target="_blank">
+          <AiFillInstagram size={30} />
+        </a>
+        <a href="https://facebook.com" target="_blank">
+          <AiFillFacebook size={30} />
+        </a>
+        <a href="https://twitter.com" target="_blank">
+          <AiFillTwitterSquare size={30} />
+        </a>
+      </div>
+      <div className="text-sm flex gap-2">
+        2023 Developed by{" "}
+        <a
+          href="https://github.com/nikolad991"
+          target="_blank"
+          className="flex  hover:text-red-400"
+        >
+          {" "}
+          <AiFillGithub size={18} /> nikolad991
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { BiSolidMoviePlay } from "react-icons/bi";
+import { useRouteError, Link } from "react-router-dom";
+import SearchBar from "../components/SearchBar";
 
 const ErrorPage = () => {
+  const { data, status, statusText, error } = useRouteError();
   return (
-    <div>ErrorPage</div>
-  )
-}
+    <div className=" min-h-screen  flex flex-col  bg-neutral-600 gap-2">
+      <Link to="/">
+        <div className=" m-4 flex items-center w-fit font-bold text-2xl font-dosis border-b-4 border-t-4 border-dashed border-black bg-yellow-700 p-1">
+          <BiSolidMoviePlay /> <span>MOVIE_DB</span>
+        </div>
+      </Link>
+      <div className="flex flex-col items-center gap-5 py-20">
+        <div className="bg-neutral-300 rounded-md flex flex-col p-8 items-center">
+          <h1 className="text-5xl font-bold mt-5 p-5 rounded-md bg-red-800">
+            {status}
+          </h1>
+          <h2 className="text-xl">{statusText}</h2>
+          <p className="fs-3">
+            <span className="text-red-900">Oops!</span> Page not found.
+          </p>
+          <p className="lead">{data}</p>
+        </div>
+        <Link
+          to="/"
+          className="bg-neutral-800 px-3 py-1 text-white text-lg font-semibold rounded-md transition duration-500 hover:bg-white hover:text-neutral-800"
+        >
+          Go Home
+        </Link>
+        <p className="text-white text-lg">Or type bellow to search for any movie</p>
+        <SearchBar />
+      </div>
+      
+    </div>
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
